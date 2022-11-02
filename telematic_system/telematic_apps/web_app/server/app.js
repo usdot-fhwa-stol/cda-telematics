cb55061a4f8368d0eb45c9dd2967ab81eb8fe0fd
itabrl2 <samir.manafzadehtabriz@leidos.com>
2022-11-02 04:23:40 +0000

    remove import

--- telematic_system/telematic_apps/web_app/server/app.js
+++ telematic_system/telematic_apps/web_app/server/app.js
@@ -21,8 +21,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
-var unitsRouter = require("./routes/units");
 
 var app = express();
 const cors = require('cors')
@@ -51,7 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/units", unitsRouter);
 require("./routes/events.router")(app);
 require("./routes/locations.router")(app);
 require("./routes/units.router")(app);
