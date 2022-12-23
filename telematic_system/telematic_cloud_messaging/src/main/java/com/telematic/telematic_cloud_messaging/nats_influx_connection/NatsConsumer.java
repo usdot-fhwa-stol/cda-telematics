74961cee026b63c48e5a0b95b356a83bd99a77a3
Anish <Anishdeva17@gmail.com>
2022-12-23 17:17:22 -0500

    fix

--- telematic_system/telematic_cloud_messaging/src/main/java/com/telematic/telematic_cloud_messaging/nats_influx_connection/NatsConsumer.java
+++ telematic_system/telematic_cloud_messaging/src/main/java/com/telematic/telematic_cloud_messaging/nats_influx_connection/NatsConsumer.java
@@ -79,8 +79,8 @@ public class NatsConsumer {
         //message has been received
         Dispatcher d = nc.createDispatcher((msg) -> {
             String str = new String(msg.getData(), StandardCharsets.UTF_8);
-            logger.info("Entering dispatcher, Bucket type: " + influxDataWriter.config_.influx_bucket_type.toString());
-            if(influxDataWriter.config_.influx_bucket_type.equals(Config.BucketType.CLOUD)){
+            logger.info("Entering dispatcher, subscribe str: " + nats_subscribe_str);
+            if(nats_subscribe_str.equals(influxDataWriter.config_.cloud_subscription_topic)){
                 logger.info("Received cloud data");
                 influxDataWriter.publishCloudData(str);
             }
