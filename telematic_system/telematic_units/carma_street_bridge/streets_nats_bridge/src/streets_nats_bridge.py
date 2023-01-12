c7386fe303a56561e82a130c1444748afb1822df
dan-du-car <dan.du@leidos.com>
2023-01-12 10:41:55 -0500

    init

--- telematic_system/telematic_units/carma_street_bridge/streets_nats_bridge/src/streets_nats_bridge.py
+++ telematic_system/telematic_units/carma_street_bridge/streets_nats_bridge/src/streets_nats_bridge.py
@@ -313,9 +313,9 @@ class StreetsNatsBridge():
 
             # Remove topics from subscribers list that weren't called in new request
             for existing_topic in list(self.subscribers_list):
-                if (existing_topic not in requested_topics[0]):
+                if (existing_topic not in requested_topics):
                     try:
-                        self.logger.info('Trying to unsubscribe from topic: "%s"' % existing_topic)
+                        self.logger.info('Trying to unsubscribe from topic update: "%s"' % existing_topic)
                         self.subscribers_list.remove(existing_topic)
                     except:
                         self.logger.error('Unable to unsubscribe from topic: "%s" '% existing_topic)
