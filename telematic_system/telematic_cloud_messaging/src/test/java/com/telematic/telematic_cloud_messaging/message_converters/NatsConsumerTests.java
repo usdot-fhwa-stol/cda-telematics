4898c1ab94114d41633220b10af6ae04f5c55d52
dan <dan.du@leidos.com>
2023-12-07 15:10:55 -0500

    init

--- telematic_system/telematic_cloud_messaging/src/test/java/com/telematic/telematic_cloud_messaging/message_converters/NatsConsumerTests.java
+++ telematic_system/telematic_cloud_messaging/src/test/java/com/telematic/telematic_cloud_messaging/message_converters/NatsConsumerTests.java
@@ -1,7 +1,8 @@
 package com.telematic.telematic_cloud_messaging.message_converters;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.json.JSONArray;
 import org.json.JSONException;
