e34171d62b6993b418e4c15237dde3f874fe5fc0
dan <dan.du@leidos.com>
2024-01-11 21:15:05 -0500

    update

--- telematic_system/local.data.mysql.sh
+++ telematic_system/local.data.mysql.sh
@@ -1,2 +1,2 @@
 
-docker exec -i mysql mysql -utelematic -ptelematic wfd_grafana < ./scripts/database/mysql_insert_values.sql
+docker exec -i telematic_system_mysqldb_1 mysql -utelematic -ptelematic wfd_grafana < ./scripts/database/mysql_insert_values.sql
