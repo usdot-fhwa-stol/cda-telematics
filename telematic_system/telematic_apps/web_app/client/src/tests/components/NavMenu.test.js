4cb9efd6fa7ab714a669a74d668258659dc5425e
dan <dan.du@leidos.com>
2024-01-18 17:04:56 -0500

    add unit test

---
+++ telematic_system/telematic_apps/web_app/client/src/tests/components/NavMenu.test.js
@@ -0,0 +1,39 @@
+import { expect, jest, test } from "@jest/globals";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import AuthContext from "../../context/auth-context";
+import NavMenu from "../../components/layout/NavMenu";
+import MainRouter from "../../components/layout/MainRouter";
+import Layout from "../../components/layout/Layout";
+import { BrowserRouter } from "react-router-dom";
+
+test("Test NavMenu checkServerSession", async () => {
+  await act(async() => {
+    const value = {
+      user_id: 1,
+      isAuth: "true",
+      username: "test",
+      email: "test@telematic.com",
+      sessionToken: "sessionToken",
+      last_seen_at: 0,
+      org_id: "1",
+      name: "test",
+      org_name: "my-org",
+      is_admin: "1",
+      role: "Admin",
+      sessionExpiredAt: 100000,
+    };
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider value={value}>
+          <NavMenu />
+        </AuthContext.Provider>
+      </BrowserRouter>
+    );
+  });
+
+  await waitFor(() => {
+    expect(screen.getByText("Events")).toBeInTheDocument();
+});
+});
