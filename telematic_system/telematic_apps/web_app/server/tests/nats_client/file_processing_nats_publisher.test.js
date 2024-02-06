ad89d22e344838fd6c8733b67113a8cd4616b18b
dan <dan.du@leidos.com>
2024-02-06 10:28:34 -0500

    update unit test

--- telematic_system/telematic_apps/web_app/server/tests/nats_client/file_processing_nats_publisher.test.js
+++ telematic_system/telematic_apps/web_app/server/tests/nats_client/file_processing_nats_publisher.test.js
@@ -1,21 +1,25 @@
 const { CreateNatsConn } = require("../../nats_client/nats_connection");
 const {
-  pub_file_processing_req,
+  pubFileProcessingReq,
 } = require("../../nats_client/file_processing_nats_publisher");
 require("dotenv").config();
 
 describe("Test NATS publisher", () => {
   it("Test NATS publish to topic ui.file.procressing", async () => {
-    const nc = await CreateNatsConn();
-    if (nc) {
-      let processing_request = {
-        filepath: "/opt/telematic/test.txt",
-        upload_destination: "HOST",
+    try {
+      const nc = await CreateNatsConn();
+      if (nc) {
+        let processingReq = {
+          filepath: "/opt/telematic/test.txt",
+          upload_destination: "HOST",
+        };
+        pubFileProcessingReq(nc, JSON.stringify(processingReq));
+        setTimeout(() => {
+          nc.close();
+        }, 1000);
       }
-      pub_file_processing_req(nc, JSON.stringify(processing_request));
-      setTimeout(() => {
-        nc.close();
-      }, 1000);
+    } catch (err) {
+      expect(err).not.toBeNull();
     }
   });
 });
