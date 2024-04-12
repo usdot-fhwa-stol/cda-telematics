cddc6f05d38ee28093e290189974f45b6c6227b0
dan-du-car <dan.du@leidos.com>
2024-04-12 09:42:39 -0400

    update grafana aurl

--- telematic_system/env.js
+++ telematic_system/env.js
@@ -2,7 +2,7 @@
 window.env = {
   REACT_APP_MESSAGING_SERVER_URI: "https://topic-service.cav-telematics.com",
   REACT_APP_WEB_SERVER_URI: "https://ui-service.cav-telematics.com",
-  REACT_APP_GRAFANA_URI: "https://grafana.cav-telematics.com",
+  REACT_APP_GRAFANA_URI: "https://grafana.cav-telematics.com/grafana",
   REACT_APP_FILE_UPLOAD_WEB_SERVER_URI:
-    "https://ui-upload-service.cav-telematics.com/grafana",
+    "https://ui-upload-service.cav-telematics.com",
 };
