3b57f8a05f275d09e1a908d61de3850e741534df
Anish_deva <51463994+adev4a@users.noreply.github.com>
2024-06-12 13:09:47 -0400

    Wfd 545 stop service on thread crash (#226)
    * catch unindexed rosbag error
    
    * update comment
    
    * update
    
    * add stop event to nats callback thread
    
    * add stop event for both service threads

--- telematic_system/telematic_historical_data_processing/rosbag2_processing_service/rosbag2_processing_service/main.py
+++ telematic_system/telematic_historical_data_processing/rosbag2_processing_service/rosbag2_processing_service/main.py
@@ -16,32 +16,53 @@
 
 import asyncio
 from .config import Config
+from .config import ProcessingStatus
 from .service_manager import ServiceManager
-from threading import Thread
+from threading import Event, Thread
 
 
 def main():
 
-    def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+    def start_background_loop(loop: asyncio.AbstractEventLoop, stop_event: Event) -> None:
         asyncio.set_event_loop(loop)
-        loop.run_forever()
+        while not stop_event.is_set():
+            loop.run_until_complete(asyncio.sleep(0.1))
+        loop.stop()
+        loop.close()
 
     config = Config()
 
     service_manager = ServiceManager(config)
-    loop = asyncio.new_event_loop()
-    t = Thread(target=start_background_loop, args=(loop,))
-    t.start()
+    stop_event = Event()
 
-    # Schedule nats_connect to run in the background loop
-    asyncio.run_coroutine_threadsafe(service_manager.nats_connect(), loop)
+    nats_loop = asyncio.new_event_loop()
+    t1 = Thread(target=start_background_loop, args=(nats_loop, stop_event))
+    t1.start()
+
+    rosbag_processing_loop = asyncio.new_event_loop()
+    t2 = Thread(target=start_background_loop, args=(rosbag_processing_loop, stop_event))
+    t2.start()
 
 
-    main_loop = asyncio.get_event_loop()
     try:
-        main_loop.run_until_complete(service_manager.process_rosbag_queue())
+        nats_future = asyncio.run_coroutine_threadsafe(service_manager.nats_connect(), nats_loop)
+        rosbag_processing_future = asyncio.run_coroutine_threadsafe(service_manager.process_rosbag_queue(), rosbag_processing_loop)
+
+        nats_future.result()
+        rosbag_processing_future.result()
+    except Exception as e:
+
+        # If service crashed before updating processing status try update
+        if service_manager.rosbag_parser.is_processing:
+            try:
+                service_manager.update_mysql_entry(service_manager.last_processed_rosbag, ProcessingStatus.ERROR.value, "Uncaught error while processing rosbag")
+            except Exception as e:
+                service_manager.config.logger.error(f"Could not update processing status for rosbag {service_manager.last_processed_rosbag}")
+
+        stop_event.set()
     finally:
-        main_loop.close()
+        t1.join()
+        t2.join()
 
 
 if __name__ == '__main__':
